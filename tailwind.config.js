46a55fc45d724a734bfb957ef3e32f8cec284e23
James Curtis <mybastion@gmail.com>
2024-12-08 09:28:06 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,elm,ts,css,html}",
+    "./.elm-land/**/*.{js,elm,ts,css,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
